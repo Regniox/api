dd86d5f42a1b630b6d604c3ffb8c433fb5b17200
Rain <Regniox@outlook.com>
2025-08-12 15:11:02 +0800

    add some api about steam

--- api/steam.py
+++ api/steam.py
@@ -6,8 +6,25 @@ steam_bp = Blueprint('steam_bp', __name__)
 @steam_bp.route('/steam/GetPlayerSummaries/')
 def GetPlayerSummaries():
     key = request.args.get('key')
+    # FOCUS: steamids
     steamids = request.args.get('steamids')
     api_url = "https://community.steam-api.com/ISteamUser/GetPlayerSummaries/v2/"
-    r = requests.get(api_url, params={ 'key': key, 'steamids': steamids })
-    return r.json()
+    response = requests.get(api_url, params={ 'key': key, 'steamids': steamids })
+    return response.json(), 200
 
+@steam_bp.route('/steam/GetRecentlyPlayedGames/')
+def GetRecentlyPlayedGames():
+    key = request.args.get('key')
+    steamid = request.args.get('steamid')
+    api_url = "https://community.steam-api.com/IPlayerService/GetRecentlyPlayedGames/v1/"
+    response = requests.get(api_url, params={ 'key': key, 'steamid': steamid })
+    return response.json(), 200
+
+@steam_bp.route('/steam/GetOwnedGames/')
+def GetOwnedGames():
+    key = request.args.get('key')
+    steamid = request.args.get('steamid')
+    include_appinfo = request.args.get('include_appinfo')
+    api_url = "https://community.steam-api.com/IPlayerService/GetOwnedGames/v1/"
+    response = requests.get(api_url, params={ 'key': key, 'steamid': steamid, 'include_appinfo': include_appinfo })
+    return response.json(), 200
